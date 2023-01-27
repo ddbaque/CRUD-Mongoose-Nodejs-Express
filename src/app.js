7da40b5251dc1b25d6054ed1161bcb4d7dc06a30
tupapiloko <baquedavid0@gmail.com>
2023-01-27 23:38:27 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import indexRoutes from "./routes/index.routes";
 import { create } from "express-handlebars";
 import path from "path";
 import morgan from "morgan";
+import cors from "cors";
+
+const whitelist = ["http://localhost:5500/"];
 
 const app = express();
+app.use(cors({ origin: whitelist }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
